7325675ed4a8e5a9d7b3d6e6cca21a15b68af029
Dan <daniel.murphy.1@gmail.com>
2020-07-13 15:51:08 -0400

    completed style and functionality, created README

---
+++ README.md
@@ -0,0 +1,32 @@
+# Rock, Paper, Scissors Game
+
+### Summary
+
+---
+
+This app allows the user to play rock, paper, scissors against the computer. 
+
+![RPS App Screenshot](https://github.com/danielmurphy1/RockPaperScissors/blob/master/RPSScreenshot.PNG)
+
+### Instructions
+
+---
+Play Rock, Paper, Scissors here.
+
+Click one of the images on the left - either rock, paper, or scissors. The computer will select its choice and display an impage of its choice on the right. The center coloum will populate with your choice, the computer's choice, and whether you win, you lose, or you tie.
+
+### Languages and Markup Used
+
+---
+
+- HTML
+- CSS
+- Javascript
+
+Digital Clock App was created without using any libraries or frameworks.
+
+### Author
+
+---
+
+Dan Murphy
--- script.js
+++ script.js
@@ -13,9 +13,8 @@ function chooseComputerSymbol() {
   let number = Math.floor(Math.random() * 3);
   let computerSymbol = "";
   let computerChoice = document.createElement("img");
-  let h3Computer = document.getElementById("computer");
-  let paragraph = document.getElementById("computer-paragraph");
   let computerContainer = document.getElementById("computer-container");
+  
   if (computerContainer.hasChildNodes()) {
     computerContainer.removeChild(computerContainer.firstChild);
   }
@@ -38,18 +37,14 @@ function chooseComputerSymbol() {
       break;
     default:
   }
-  if (paragraph.hasChildNodes()) {
-    paragraph.remove();
-  }
-  paragraph.textContent = computerSymbol;
-  h3Computer.appendChild(paragraph);
+  outputResults(computerSymbol, "computer", "computer-paragraph");
+
   return computerSymbol;
 }
 
 function chooseUserSymbol(e) {
   let userSymbol = "";
-  let h3User = document.getElementById("user");
-  let paragraph = document.getElementById("user-paragraph");
+  
   if (e.target.classList.contains("rock")) {
     userSymbol = "rock";
   } else if (e.target.classList.contains("paper")) {
@@ -57,18 +52,14 @@ function chooseUserSymbol(e) {
   } else if (e.target.classList.contains("scissors")) {
     userSymbol = "scissors";
   }
-  if (paragraph.hasChildNodes()) {
-    paragraph.remove();
-  }
-  paragraph.textContent = userSymbol;
-  h3User.appendChild(paragraph);
+  outputResults(userSymbol, "user", "user-paragraph");
+  
   return userSymbol;
 }
 
 function compareSymbols(userSymbol, computerSymbol) {
   let result = "";
-  let paragraph = document.getElementById("result-paragraph");
-  let h3Result = document.getElementById("result");
+ 
   if (userSymbol === computerSymbol) {
     result = "It's a tie!";
   } else if (
@@ -81,9 +72,18 @@ function compareSymbols(userSymbol, computerSymbol) {
     result = "The computer won!";
   }
 
-  if (paragraph.hasChildNodes()) {
+  outputResults(result, "result", "result-paragraph");
+  
+  return result;
+}
+
+function outputResults(outPut, headingType, paragraphType){
+  let paragraph = document.getElementById(paragraphType);
+  let resultHeading = document.getElementById(headingType);
+  if (paragraph.hasChildNodes()){
     paragraph.remove();
   }
-  paragraph.textContent = result;
-  h3Result.appendChild(paragraph);
+  paragraph.textContent = outPut;
+  resultHeading.appendChild(paragraph);
 }
+
